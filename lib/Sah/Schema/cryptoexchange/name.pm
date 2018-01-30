4652806874eb3b5c1e5e680c5a2edf66e0ade0ab
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-01-30 21:07:54 +0700

    Add schema: cryptoexchange::name

---
+++ lib/Sah/Schema/cryptoexchange/name.pm
@@ -0,0 +1,12 @@
+package Sah::Schema::cryptoexchange::name;
+
+# DATE
+# VERSION
+
+our $schema = [str => {
+    summary => 'Cryptocurrency exchange name',
+    'x.completion' => 'cryptoexchange_name',
+}, {}];
+
+1;
+# ABSTRACT:
